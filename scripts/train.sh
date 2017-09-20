d55d1e8e9285efedd12081bd8fbc2b1b79dadffb
Adam <ajwieme@gmail.com>
2017-09-20 17:49:32 -0600

    reflecting folder name change in bash scripts

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,6 +6,6 @@ source ~/virtual-envs/OpenNMT-py-env/bin/activate
 LANG="$1"
 SETTING="$2"
 
-python ~/sigmorphon_data/scripts/conll2onmt.py ~/sigmorphon_data/data/"$LANG"-train-"$SETTING" ~/sigmorphon_data/data/"$LANG"-dev ~/sigmorphon_data/answers/"$LANG"-uncovered-test ~/sigmorphon_data/ONMT_data "$LANG"-"$SETTING"
-python ~/OpenNMT-py/preprocess.py -train_src  ~/sigmorphon_data/ONMT_data/train-"$LANG"-"$SETTING"-src.txt -train_tgt  ~/sigmorphon_data/ONMT_data/train-"$LANG"-"$SETTING"-tgt.txt -valid_src  ~/sigmorphon_data/ONMT_data/val-"$LANG"-"$SETTING"-src.txt -valid_tgt  ~/sigmorphon_data/ONMT_data/val-"$LANG"-"$SETTING"-tgt.txt -save_data ~/sigmorphon_data/models/"$LANG"-"$SETTING"
-python ~/OpenNMT-py/train.py -data ~/sigmorphon_data/models/"$LANG"-"$SETTING" -save_model ~/sigmorphon_data/models/"$LANG"-"$SETTING"-model
+python ~/sigmorphon-data/scripts/conll2onmt.py ~/sigmorphon-data/data/"$LANG"-train-"$SETTING" ~/sigmorphon-data/data/"$LANG"-dev ~/sigmorphon-data/answers/"$LANG"-uncovered-test ~/sigmorphon-data/ONMT_data "$LANG"-"$SETTING"
+python ~/OpenNMT-py/preprocess.py -train_src  ~/sigmorphon-data/ONMT_data/train-"$LANG"-"$SETTING"-src.txt -train_tgt  ~/sigmorphon-data/ONMT_data/train-"$LANG"-"$SETTING"-tgt.txt -valid_src  ~/sigmorphon-data/ONMT_data/val-"$LANG"-"$SETTING"-src.txt -valid_tgt  ~/sigmorphon-data/ONMT_data/val-"$LANG"-"$SETTING"-tgt.txt -save_data ~/sigmorphon-data/models/"$LANG"-"$SETTING"
+python ~/OpenNMT-py/train.py -data ~/sigmorphon-data/models/"$LANG"-"$SETTING" -save_model ~/sigmorphon-data/models/"$LANG"-"$SETTING"-model
