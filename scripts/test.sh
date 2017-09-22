f08d00176cb3e2e175875ca49b18c57e2803b9ec
Adam <ajwieme@gmail.com>
2017-09-22 11:48:33 -0600

    test needs ROOT too, dumb dumb

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,10 @@
 #! /bin/sh
 # Test a traned model on some data
 
-source ~/virtual-envs/OpenNMT-py-env/bin/activate
-
 ROOT="$1"
 MODEL="$2"
 LANG="$3"
 SETTING="$4"
 
-python ~/OpenNMT-py/translate.py -model "$MODEL" -src ~/sigmorphon_data/ONMT_data/test-"$LANG"-"$SETTING"-src.txt -tgt ~/sigmorphon_data/ONMT_data/test-"$LANG"-"$SETTING"-tgt.txt  -output ~/sigmorphon_data/predictions/"$LANG-$SETTING"-pred.txt -replace_unk -verbose
-python ~/sigmorphon_data/scripts/evalm.py --gold ~/sigmorphon_data/ONMT_data/test-"$LANG"-"$SETTING"-tgt.txt --guess ~/sigmorphon_data/predictions/"$LANG-$SETTING"-pred.txt
+python "$ROOT"/OpenNMT-py/translate.py -model "$MODEL" -src "$ROOT"/sigmorphon_data/ONMT_data/test-"$LANG"-"$SETTING"-src.txt -tgt "$ROOT"/sigmorphon_data/ONMT_data/test-"$LANG"-"$SETTING"-tgt.txt  -output "$ROOT"/sigmorphon_data/predictions/"$LANG-$SETTING"-pred.txt -replace_unk -verbose
+python "$ROOT"/sigmorphon_data/scripts/evalm.py --gold "$ROOT"/sigmorphon_data/ONMT_data/test-"$LANG"-"$SETTING"-tgt.txt --guess "$ROOT"/sigmorphon_data/predictions/"$LANG-$SETTING"-pred.txt
